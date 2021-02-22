d8c085af781b373d264ed391f5fff3748d3df048
Herman Stander <her.stander@gmail.com>
2021-02-22 15:50:29 +0200

    added certs

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+const fs = require("fs");
 const SMTPServer = require("smtp-server").SMTPServer;
 const parser = require("mailparser").simpleParser;
-const hostname = "127.0.0.1";
 const port = 2525;
 
+// Congratulations! Your certificate and chain have been saved at:
+//    /etc/letsencrypt/live/smtp.snapcatch.org/fullchain.pem
+//    Your key file has been saved at:
+//    /etc/letsencrypt/live/smtp.snapcatch.org/privkey.pem
+//    Your cert will expire on 2021-05-23. To obtain a new or tweaked
+//    version of this certificate in the future, simply run certbot
+//    again. To non-interactively renew *all* of your certificates, run
+//    "certbot renew"
+
 const server = new SMTPServer({
+  key: fs.readFileSync("/etc/letsencrypt/live/smtp.snapcatch.org/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/smtp.snapcatch.org/cert.pem"),
   onAuth(auth, session, callback) {
     if (
       auth.username !== "postmaster@snapcatch.org" ||
@@ -17,20 +28,20 @@ const server = new SMTPServer({
     if (address.address !== "postmaster@snapcatch.org") {
       return callback(new Error("You are not allowed to send mail"));
     }
-    return callback(null, {ok: true}); // Accept the address
+    return callback(null, { ok: true }); // Accept the address
   },
-    onData(stream, session, callback) {
-        // stream.pipe(process.stdout); // print message to console
-        // stream.on("end", callback);
-        parser(stream, {}, (err, parsed) => {
-          if (err) {
-              console.log("Error:", err);
-          }
-        //   Do something with parsed. Send somewhere?
-          console.log(parsed);
-        });
-        stream.on("end", callback);
-        // return callback(null, { ok: true });
+  onData(stream, session, callback) {
+    // stream.pipe(process.stdout); // print message to console
+    // stream.on("end", callback);
+    parser(stream, {}, (err, parsed) => {
+      if (err) {
+        console.log("Error:", err);
+      }
+      //   Do something with parsed. Send somewhere?
+      console.log(parsed);
+    });
+    stream.on("end", callback);
+    // return callback(null, { ok: true });
   },
   onConnect(session, callback) {
     callback(null, { ok: true });
