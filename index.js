b22ca4c5819b61e6e7e56e6cbbff838e32065be5
Herman Stander <her.stander@gmail.com>
2021-02-22 17:30:20 +0200

    Fixed tls

--- index.js
+++ index.js
@@ -3,22 +3,10 @@ const SMTPServer = require("smtp-server").SMTPServer;
 const parser = require("mailparser").simpleParser;
 const port = 2525;
 
-// Congratulations! Your certificate and chain have been saved at:
-//    /etc/letsencrypt/live/smtp.snapcatch.org/fullchain.pem
-//    Your key file has been saved at:
-//    /etc/letsencrypt/live/smtp.snapcatch.org/privkey.pem
-//    Your cert will expire on 2021-05-23. To obtain a new or tweaked
-//    version of this certificate in the future, simply run certbot
-//    again. To non-interactively renew *all* of your certificates, run
-//    "certbot renew"
-
 const server = new SMTPServer({
   secure: false,
-//   key: fs.readFileSync("/etc/letsencrypt/live/smtp.snapcatch.org/privkey.pem"),
-//   cert: fs.readFileSync("/etc/letsencrypt/live/smtp.snapcatch.org/cert.pem"),
+  disabledCommands: ['STARTTLS'],
   onAuth(auth, session, callback) {
-      console.log(auth);
-      console.log(session);
     if (
       auth.username !== "postmaster@snapcatch.org" ||
       auth.password !== "123"
@@ -28,14 +16,13 @@ const server = new SMTPServer({
     callback(null, { user: 1 });
   },
   onMailFrom(address, session, callback) {
-      console.log(session);
+    console.log(session);
     if (address.address !== "postmaster@snapcatch.org") {
       return callback(new Error("You are not allowed to send mail"));
     }
     return callback(null, { ok: true }); // Accept the address
   },
   onData(stream, session, callback) {
-
     // stream.pipe(process.stdout); // print message to console
     // stream.on("end", callback);
     parser(stream, {}, (err, parsed) => {
@@ -49,7 +36,6 @@ const server = new SMTPServer({
     // return callback(null, { ok: true });
   },
   onConnect(session, callback) {
-      console.log(session);
     callback(null, { ok: true });
   },
 });
